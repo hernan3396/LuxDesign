43e7dbcaccff2db33e8784f98bc5fc383bf24569
hernan3396 <hernan3396@gmail.com>
2020-08-29 01:13:06 -0300

    changed trabajos cards to grid style_v1

--- resources/views/trabajos.blade.php
+++ resources/views/trabajos.blade.php
@@ -24,4 +24,16 @@ Trabajos
 
     </div>
 
+
+    @foreach ($categoria->trabajos as $trabajo)
+    <div class="" style="display:grid margin-bottom:5px">
+        <a href="{{ Storage::disk('s3')->url('trabajos/' . $trabajo->imagen) }}" data-lightbox="roadtrip">
+
+            <img class="card-img-top hover-shadow" src="{{ Storage::disk('s3')->url('trabajos/' . $trabajo->imagen) }}"
+                alt="{{ $trabajo->nombre }}" style="" data-lightbox="roadtrip"></a>
+
+        <p class="card-text" style="text-align: center">{{ $trabajo->nombre }}</p>
+    </div>
+    @endforeach
+
     @endsection
