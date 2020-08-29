3bdd1eaef341f0e4ad7f855b7fb79dc6a5e1fcf9
hernan3396 <hernan3396@gmail.com>
2020-08-29 01:18:43 -0300

    changed trabajos cards to grid_v2

--- resources/views/trabajos.blade.php
+++ resources/views/trabajos.blade.php
@@ -8,23 +8,6 @@ Trabajos
 
 <div class="container">
 
-    <div class="row justify-content-between">
-
-        @foreach ($categoria->trabajos as $trabajo)
-        <div class="col-sm-4" style="margin-bottom:5px">
-            <a href="{{ Storage::disk('s3')->url('trabajos/' . $trabajo->imagen) }}" data-lightbox="roadtrip">
-
-                <img class="card-img-top hover-shadow"
-                    src="{{ Storage::disk('s3')->url('trabajos/' . $trabajo->imagen) }}" alt="{{ $trabajo->nombre }}"
-                    style="height: 200px" data-lightbox="roadtrip"></a>
-
-            <p class="card-text" style="text-align: center">{{ $trabajo->nombre }}</p>
-        </div>
-        @endforeach
-
-    </div>
-
-
     @foreach ($categoria->trabajos as $trabajo)
     <div class="" style="display:grid margin-bottom:5px">
         <a href="{{ Storage::disk('s3')->url('trabajos/' . $trabajo->imagen) }}" data-lightbox="roadtrip">
